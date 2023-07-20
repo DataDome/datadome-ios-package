36ef221761620f4dd3b0dd37842082729087c833
Alexandre Brispot <alexandre.brispot@datadome.co>
2023-07-20 21:00:54 +0200

    Update package version to 3.2.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DataDomeSDK",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -19,8 +19,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .binaryTarget(
             name: "DataDomeSDK",
-            url: "https://package.datadome.co/ios/DataDomeSDK-3.1.0.zip",
-            checksum: "5c158cacbe2ad6bf6f95f4b4f84a7bc63e6eca62f2512bb660e3b637cbb732d8"
+            url: "https://package.datadome.co/ios/DataDomeSDK-3.2.1.zip",
+            checksum: "e1b85f64516056a150d7d6fd01ebf435a63f6d02e7784b20e8c9188e0e714215"
         )
     ]
 )
